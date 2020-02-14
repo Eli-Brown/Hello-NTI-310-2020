fccd3ac179500b1fe134da51c6e89f804ac85061
Eli <vincent.brown@seattlecolleges.edu>
2020-02-13 18:38:17 -0800

    Update ldap_client.sh

--- ldap_client.sh
+++ ldap_client.sh
@@ -14,9 +14,9 @@ apt-get install nfs-client nfs-common -y
 
 
 
-showmount -e 10.138.0.2 # where $ipaddress is the ip of your nfs server
+showmount -e 10.128.0.2 # where $ipaddress is the ip of your nfs server
 mkdir /mnt/test 
-echo "10.138.0.2:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+echo "10.128.0.2:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 mount -a
 
 
