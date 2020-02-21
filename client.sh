0328292d9f0abfcf1b9a5933f138e4b93c383a50
Eli <vincent.brown@seattlecolleges.edu>
2020-02-20 19:35:10 -0800

    Thursday 2/20/20

---
+++ client.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+apt-get install nfs-client nfs-common -y
+
+
+showmount -e 10.128.0.4 # where $ipaddress is the ip of your nfs server
+
+mkdir /mnt/test 
+
+echo "10.128.0.4:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+
+mount -a
