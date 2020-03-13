bb0c2b8ef48804d7548e95970fdf982b6dcdb3b7
Eli <vincent.brown@seattlecolleges.edu>
2020-03-12 19:10:28 -0700

    Create spinup.sh

---
+++ LogServer/spinup.sh
@@ -0,0 +1,11 @@
+#!bash.sh
+
+gcloud compute instances create rsyslog-server2 \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central1-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/path/to/script.sh \
+--private-network-ip=10.128.0.2
