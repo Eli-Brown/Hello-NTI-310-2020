28a2d00bce3dedb7f2f7f26dcd5b26d60ce5f5a3
Eli <vincent.brown@seattlecolleges.edu>
2020-03-12 19:16:56 -0700

    Update spinup.sh

--- LogServer/spinup.sh
+++ LogServer/spinup.sh
@@ -7,5 +7,5 @@ gcloud compute instances create rsyslog-server2 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/Hello-NTI-310-2020/LogServer/spinup.sh \
+--metadata-from-file startup-script=/home/Hello-NTI-310-2020/LogServer/spinup.sh \
 --private-network-ip=10.128.0.2
