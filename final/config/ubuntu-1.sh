24247a346351b6e2137b64920f319112c6d20c97
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 15:57:46 -0700

    Update ubuntu-1.sh

--- final/config/ubuntu-1.sh
+++ final/config/ubuntu-1.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+gcloud compute instances create ubuntu-1 \
+--image-family ubuntu-1804-lts \
+--image-project ubuntu-os-cloud \
+--zone us-central1-a	 \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/ \
+--private-network-ip=10.128.0.10
