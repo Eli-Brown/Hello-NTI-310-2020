d78c57d05bc144d9c237b609cba9fe26ee867604
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 14:26:25 -0700

    Update ldap_client-final.sh

--- final/config/ldap_client-final.sh
+++ final/config/ldap_client-final.sh
@@ -2,7 +2,40 @@
 #!/bin/bash
 # note, this is only part of the ldap CLIENT/server install script.  These are the relevent sed lines.
 
+# put this at the beginning of each of your scripts
+# it will remove the centos7 repos, which are flakey right now
+# and put in our (more stable) repositories.
 
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+
+
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://34.71.91.10/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://34.71.91.10/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://34.71.91.10/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://34.71.91.10/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+
+echo "*.info;mail.none;authpriv.none;cron.none   @logserv" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+#Important: this should be the internal not external IP of the server or the dns name of your server.
 
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
