e86ee5769292ca28eaebc47b9338a5df4ba46224
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 14:32:15 -0700

    Update nfs-client.sh

--- final/config/nfs-client.sh
+++ final/config/nfs-client.sh
@@ -32,9 +32,6 @@ gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 
-echo "*.info;mail.none;authpriv.none;cron.none   @logserv" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
-#Important: this should be the internal not external IP of the server or the dns name of your server.
-
 apt-get install nfs-client nfs-common -y
 showmount -e 10.128.0.4 # where $ipaddress is the ip of your nfs server
 mkdir /mnt/test 
