5ccfd44e8aac38a84cd86b4d4d030386994f10d3
Eli <vincent.brown@seattlecolleges.edu>
2020-03-19 15:39:34 -0700

    Update rsyslog-client.sh

--- final/config/rsyslog-client.sh
+++ final/config/rsyslog-client.sh
@@ -31,5 +31,5 @@ gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 
-echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog-server" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+echo "*.info;mail.none;authpriv.none;cron.none   @10.128.0.10" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
 #Important: this should be the internal not external IP of the server or the dns name of your server.
