3c20cf440c53b8c8f9cc6898c5a3400bb4f33417
Eli <vincent.brown@seattlecolleges.edu>
2020-03-19 15:37:36 -0700

    Update ubuntu-2.sh

--- final/config/ubuntu-2.sh
+++ final/config/ubuntu-2.sh
@@ -1,34 +1,4 @@
 #/bin/bash
-# put this at the beginning of each of your scripts
-# it will remove the centos7 repos, which are flakey right now
-# and put in our (more stable) repositories.
-
-for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
-
-
-echo "[nti-310-epel]
-name=NTI310 EPEL
-baseurl=http://34.71.91.10/epel
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
-echo "[nti-310-base]
-name=NTI310 BASE
-baseurl=http://34.71.91.10/base
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
-echo "[nti-310-extras]
-name=NTI310 EXTRAS
-baseurl=http://34.71.91.10/extras/
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
-echo "[nti-310-updates]
-name=NTI310 UPDATES
-baseurl=http://34.71.91.10/updates/
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 apt-get install nfs-client git -y
 
@@ -40,5 +10,5 @@ echo "10.128.0.18:/var/nfsshare/testing        /mnt/test       nfs     defaults 
 mount -a
 
 
-echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog-server" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+echo "*.info;mail.none;authpriv.none;cron.none   @10.128.0.10" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
 #Important: this should be the internal not external IP of the server or the dns name of your server.
