86cc2c0ec18c3cde8c827ec0551f3d1e1babdc2d
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 21:26:06 -0700

    Update spinup-rsyslog-server.sh

--- final/spinup-rsyslog-server.sh
+++ final/spinup-rsyslog-server.sh
@@ -32,5 +32,7 @@ enabled=1" >> /etc/yum.repos.d/local-repo.repo
 ## create ryslog-server 1st via Gshell ##
 yum install git 
 git clone https://github.com/Eli-Brown/Hello-NTI-310-2020.git
-echo "*.info;mail.none;authpriv.none;cron.none   @logserv" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+
+echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog-server" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+
 #Important: this should be the internal not external IP of the server or the dns name of your server.
