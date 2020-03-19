f41f7bede47fa5f4694082356c307e0ab19fddf4
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 19:20:26 -0700

    Update nfs-client.sh

--- final/config/nfs-client.sh
+++ final/config/nfs-client.sh
@@ -38,3 +38,6 @@ mkdir /mnt/test
 
 echo "10.128.0.18:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 mount -a
+
+echo "*.info;mail.none;authpriv.none;cron.none   @logserv" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+#Important: this should be the internal not external IP of the server or the dns name of your server.
