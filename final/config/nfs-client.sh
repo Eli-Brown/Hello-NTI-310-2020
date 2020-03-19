b18265fd284d1b54c0610a7a1f63d44c78cbac18
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 18:47:00 -0700

    Update nfs-client.sh

--- final/config/nfs-client.sh
+++ final/config/nfs-client.sh
@@ -32,9 +32,9 @@ gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 
-apt-get install nfs-client nfs-common -y
-showmount -e 10.128.0.4 # where $ipaddress is the ip of your nfs server
+apt-get install ldap nfs-client nfs-common -y
+showmount -e 10.128.0.18 # where $ipaddress is the ip of your nfs server
 mkdir /mnt/test 
 
-echo "10.128.0.221:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+echo "10.128.0.18:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 ount -a
