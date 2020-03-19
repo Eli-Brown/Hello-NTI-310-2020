6e19a921f257f2603f007aac6e6afd41e793007d
Eli <vincent.brown@seattlecolleges.edu>
2020-03-18 17:19:26 -0700

    Update ubuntu-2.sh

--- final/config/ubuntu-2.sh
+++ final/config/ubuntu-2.sh
@@ -1,9 +1,9 @@
-gcloud compute instances create ubuntu-2 \
---image-family centos-7 \
---image-project centos-cloud \
---zone us-east1-b \
---tags "http-server","https-server" \
---machine-type f1-micro \
---scopes cloud-platform \
---metadata-from-file startup-script=/path/to/script.sh \
---private-network-ip=10.128.0.11
+#/bin/bash
+
+
+apt-get install nfs-client
+
+showmount -e 10.128.0.18# where $ipaddress is the ip of your nfs server
+mkdir /mnt/test 
+echo "10.128.0.18:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+mount -a
