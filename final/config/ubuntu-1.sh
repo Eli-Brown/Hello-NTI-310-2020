aa0ede5fa4c22fbd1e9c332fbb0453ef4e18408d
Eli <vincent.brown@seattlecolleges.edu>
2020-03-22 15:01:14 -0700

    Update ubuntu-1.sh

--- final/config/ubuntu-1.sh
+++ final/config/ubuntu-1.sh
@@ -46,5 +46,5 @@ ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,
 while read line; do echo "$line" | debconf-set-selections; done < /tmp/ldap_debconf
 
 
-echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog-server" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+echo "*.info;mail.none;authpriv.none;cron.none   @10.128.0.10" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
 #Important: this should be the internal not external IP of the server or the dns name of your server.
